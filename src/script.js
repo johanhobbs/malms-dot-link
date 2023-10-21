e70bcf7042c256b058b855c76191302a9dc94257
johanhobbs <johanhobbsnz@gmail.com>
2023-10-21 18:33:44 +1300

    Converted link list in header to an array of objectsthat populate the navigation element automatically

--- src/script.js
+++ src/script.js
@@ -1,16 +1,26 @@
-const header = document.getElementsByClassName("header")
-const navigator = document.getElementById("navigator")
+const header = document.getElementsByClassName("header");
+const navigator = document.getElementById("navigator");
 
 class linkContents {
-    constructor(linkText, linkPath){
-        this.linkText = linkText;
-        this.linkPath = linkPath;
-    }
+  constructor(linkText, linkPath) {
+    this.linkText = linkText;
+    this.linkPath = linkPath;
+  }
 }
 
-let home = new linkContents('Home', 'index.html');
-let about = new linkContents('About us', 'about.html')
-let game = new linkContents('Minigame', 'game.html')
-let inventory = new linkContents('Inventory', 'inventory.html')
-let rules = new linkContents('rules', 'rules.html')
+let home = new linkContents("Home", "index.html");
+let about = new linkContents("About us", "about.html");
+let game = new linkContents("Minigame", "game.html");
+let inventory = new linkContents("Inventory", "inventory.html");
+let rules = new linkContents("Rules", "rules.html");
+let login = new linkContents("Login", "login.html");
 
+let navLinks = [home, about, game, inventory, rules, login];
+
+navLinks.forEach((navLink) => {
+  console.log(`${navLink.linkText} at ${navLink.linkPath}`);
+  navigator.insertAdjacentHTML(
+    "beforeend",
+    `<li><a href="${navLink.linkPath}">${navLink.linkText}</a></li>`
+  );
+});
